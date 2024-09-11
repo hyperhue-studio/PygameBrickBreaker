30d4c8b02de4354c796ddf28f12e7398e7a93ac3
Miguel M <nanopizzafuturo@gmail.com>
2024-09-10 22:22:38 -0600

    Code was optimized

--- brickbreaker.py
+++ brickbreaker.py
@@ -14,7 +14,6 @@ pygame.init()
 # Configuraciones generales
 # ===================
 
-# Definición de colores
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 ROJO = (255, 0, 0)
@@ -24,13 +23,11 @@ MORADO = (255, 0, 255)
 GRIS = (128, 128, 128)
 AMARILLO = (255, 255, 0)
 
-# Configuración de la pantalla
 ANCHO = 800
 ALTO = 600
 PANTALLA = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Brick Breaker")
 
-# Definición de variables globales
 VIDAS = 3
 PUNTUACION = 0
 NIVEL = 1
@@ -54,19 +51,15 @@ class Bloque(pygame.sprite.Sprite):
     def explotar(self):
         bloques_a_destruir = []
         for bloque in bloques:
-            # Verificar bloques arriba
             if self.rect.top - bloque.rect.bottom <= radio and abs(bloque.rect.centerx - self.rect.centerx) <= self.rect.width / 2:
                 if bloque.tipo != "metal":
                     bloques_a_destruir.append(bloque)
-            # Verificar bloques abajo
             if bloque.rect.top - self.rect.bottom <= radio and abs(bloque.rect.centerx - self.rect.centerx) <= self.rect.width / 2:
                 if bloque.tipo != "metal":
                     bloques_a_destruir.append(bloque)
-            # Verificar bloques a la izquierda
             if self.rect.left - bloque.rect.right <= radio and abs(bloque.rect.centery - self.rect.centery) <= self.rect.height / 2:
                 if bloque.tipo != "metal":
                     bloques_a_destruir.append(bloque)
-            # Verificar bloques a la derecha
             if bloque.rect.left - self.rect.right <= radio and abs(bloque.rect.centery - self.rect.centery) <= self.rect.height / 2:
                 if bloque.tipo != "metal":
                     bloques_a_destruir.append(bloque)
@@ -78,20 +71,18 @@ class Bloque(pygame.sprite.Sprite):
         if potenciador == "Duplicar Pelota":
             ball2 = Bola(BLANCO, 10)
             ball2.rect.topleft = bola.rect.topleft
-            ball2.velocidad = [random.randint(2, 5), random.randint(-5, 5)]  # Velocidad aleatoria
+            ball2.velocidad = [random.randint(2, 5), random.randint(-5, 5)]
 
             todos_los_sprites.add(ball2)
             bloques_poder.add(ball2)
             bolas.add(ball2)
 
         elif potenciador == "Alargar Base":
-            # Se modifican las dimensiones de la paleta
             paleta.image = pygame.Surface([200, 20])
             paleta.image.fill(ROJO)
             paleta.rect = paleta.image.get_rect(midbottom=(ANCHO//2, ALTO))
 
         elif potenciador == "4 Pelotas":
-            # Se crean 3 nuevas pelotas
             ball2 = Bola(BLANCO, 10)
             ball2.rect.topleft = bola.rect.topleft
             ball2.velocidad = [random.randint(2, 5), random.randint(-5, 5)]
@@ -115,7 +106,6 @@ class Bloque(pygame.sprite.Sprite):
             bolas.add(ball4)
 
         elif potenciador == "Bolas Rapidas":
-            #Se aumenta la velocidad de todas las bolas
             for ball in bolas:
                 ball.velocidad = [random.randint(5, 10), random.randint(-10, 10)]
             for ball in bloques_poder:
@@ -151,8 +141,8 @@ class Bola(pygame.sprite.Sprite):
             self.velocidad[1] = -self.velocidad[1]
 
         if self.rect.bottom >= ALTO:
-            self.kill()  # Elimina la bola del grupo
-            if not bloques_poder:  # Si no hay bolas en juego
+            self.kill()
+            if not bloques_poder:
                 quitar_vida()
 
 class Paleta(pygame.sprite.Sprite):
@@ -173,9 +163,6 @@ class Paleta(pygame.sprite.Sprite):
         self.rect.right = min(ANCHO, self.rect.right)
 
     def sigue_bola(self, bola):
-        """
-        Hace que la paleta siga la bola en el eje X.
-        """
         if bola.rect.centerx > self.rect.centerx:
             self.rect.move_ip(min(10, bola.rect.centerx - self.rect.centerx), 0)
         elif bola.rect.centerx < self.rect.centerx:
@@ -212,8 +199,8 @@ def reset_bola_paleta():
     bola.rect.topleft = ((ANCHO - radio*2) // 2, ALTO - paleta.rect.height - radio*2 - 10)
     bola.velocidad = [random.randint(2, 5), random.randint(-5, 5)]
     paleta.rect.midbottom = (ANCHO//2, ALTO)
-    todos_los_sprites.add(bola)   # Agregar la bola al grupo de sprites
-    bolas.add(bola)               # Agregar la bola al grupo de bolas
+    todos_los_sprites.add(bola)
+    bolas.add(bola)
 
 
 def game_over():
@@ -265,7 +252,6 @@ def menu():
 
         pygame.display.update()
 
-# Inicialización de grupos de sprites
 bloques = pygame.sprite.Group()
 bola = Bola(BLANCO, 10)
 paleta = Paleta(ROJO, 120, 20)
@@ -275,11 +261,10 @@ reset()
 
 reloj = pygame.time.Clock()
 
-# Bucle principal del juego para solo
 def main():
     global PUNTUACION, VIDAS, NIVEL
 
-    reset_bola_paleta()  # Asegurarse de que la bola y la paleta estén en sus posiciones iniciales
+    reset_bola_paleta()
     generar_bloques()
     while VIDAS > 0:
         for evento in pygame.event.get():
@@ -288,67 +273,54 @@ def main():
 
         todos_los_sprites.update()
 
-        # Lógica de colisión
-        # Primero, colisión con la bola principal
         if pygame.sprite.collide_rect(bola, paleta):
             bola.velocidad[1] = -bola.velocidad[1]
 
-        # Luego, colisión con las bolas adicionales
         for ball in bloques_poder:
             if pygame.sprite.collide_rect(ball, paleta):
                 ball.velocidad[1] = -ball.velocidad[1]
 
         bloques_colisionados = pygame.sprite.spritecollide(bola, bloques, False)
         for bloque in bloques_colisionados:
-            # Cambiar dirección de la bola al colisionar con un bloque
             bola.velocidad[1] = -bola.velocidad[1]
             
-            # Si es un bloque "bomba", explota y se destruye
             if bloque.tipo == "bomba":
                 bloque.explotar()
                 bloque.kill()
                 continue
 
-            # Si es un bloque "potenciador", se destruye y se activa el potenciador
             if bloque.tipo == "potenciador":
                 bloque.activar_potenciador()
                 bloque.kill()
                 continue
 
-            # Si es un bloque "metal", no hacer nada más
             if bloque.tipo == "metal":
                 continue
             
-            # Reducir impactos restantes del bloque y eliminar si es necesario
             bloque.impactos -= 1
             if bloque.impactos <= 0:
                 bloque.kill()
                 multiplicador = {"normal": 10, "duro": 30, "metal": 50, "bomba": 50, "potenciador": 100}
                 PUNTUACION += multiplicador[bloque.tipo]
 
-        # Agregar lógica de colisión para bolas adicionales
         for ball in bloques_poder:
             bloques_colisionados_adicionales = pygame.sprite.spritecollide(ball, bloques, False)
             for bloque in bloques_colisionados_adicionales:
                 ball.velocidad[1] = -ball.velocidad[1]
                 
-                # Si es un bloque "bomba", explota y se destruye
                 if bloque.tipo == "bomba":
                     bloque.explotar()
                     bloque.kill()
                     continue
 
-                # Si es un bloque "potenciador", se destruye y se activa el potenciador
                 if bloque.tipo == "potenciador":
                     bloque.activar_potenciador()
                     bloque.kill()
                     continue
 
-                # Si es un bloque "metal", no hacer nada más
                 if bloque.tipo == "metal":
                     continue
 
-                # Reducir los impactos en el bloque y eliminarlo si es necesario
                 bloque.impactos -= 1
                 if bloque.impactos == 0:
                     bloque.kill()
@@ -358,7 +330,6 @@ def main():
 
         PANTALLA.fill(NEGRO)
 
-        # Dibujar y actualizar bloques
         bloques.draw(PANTALLA)
         todos_los_sprites.draw(PANTALLA)
 
@@ -369,88 +340,73 @@ def main():
         pygame.display.flip()
         reloj.tick(60)
 
-        if VIDAS == 1 and bola.rect.top >= ALTO:  # Si la vida es 1, termina el juego
+        if VIDAS == 1 and bola.rect.top >= ALTO:
             quitar_vida()
-        elif VIDAS > 1 and bola.rect.top >= ALTO:  # Verifica si hay más de 1 vida antes de restar
+        elif VIDAS > 1 and bola.rect.top >= ALTO:
             quitar_vida()
 
-# Bucle principal del juego para solo
 def mainAI():
     global PUNTUACION, VIDAS, NIVEL
 
-    reset_bola_paleta()  # Asegurarse de que la bola y la paleta estén en sus posiciones iniciales
+    reset_bola_paleta()
     generar_bloques()
     while VIDAS > 0:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 pygame.quit()
 
-        # Hacer que la paleta siga la bola
         paleta.sigue_bola(bola)
 
         todos_los_sprites.update()
 
-        # Lógica de colisión
-        # Primero, colisión con la bola principal
         if pygame.sprite.collide_rect(bola, paleta):
             bola.velocidad[1] = -bola.velocidad[1]
 
-        # Luego, colisión con las bolas adicionales
         for ball in bloques_poder:
             if pygame.sprite.collide_rect(ball, paleta):
                 ball.velocidad[1] = -ball.velocidad[1]
 
         bloques_colisionados = pygame.sprite.spritecollide(bola, bloques, False)
         for bloque in bloques_colisionados:
-            # Cambiar dirección de la bola al colisionar con un bloque
             bola.velocidad[1] = -bola.velocidad[1]
             
-            # Si es un bloque "bomba", explota y se destruye
             if bloque.tipo == "bomba":
                 bloque.explotar()
                 bloque.kill()
                 continue
 
-            # Si es un bloque "potenciador", se destruye y se activa el potenciador
             if bloque.tipo == "potenciador":
                 bloque.activar_potenciador()
                 bloque.kill()
                 continue
 
-            # Si es un bloque "metal", no hacer nada más
             if bloque.tipo == "metal":
                 continue
             
-            # Reducir impactos restantes del bloque y eliminar si es necesario
             bloque.impactos -= 1
             if bloque.impactos <= 0:
                 bloque.kill()
                 multiplicador = {"normal": 10, "duro": 30, "metal": 50, "bomba": 50, "potenciador": 100}
                 PUNTUACION += multiplicador[bloque.tipo]
 
-        # Agregar lógica de colisión para bolas adicionales
         for ball in bloques_poder:
             bloques_colisionados_adicionales = pygame.sprite.spritecollide(ball, bloques, False)
             for bloque in bloques_colisionados_adicionales:
                 ball.velocidad[1] = -ball.velocidad[1]
                 
-                # Si es un bloque "bomba", explota y se destruye
                 if bloque.tipo == "bomba":
                     bloque.explotar()
                     bloque.kill()
                     continue
 
-                # Si es un bloque "potenciador", se destruye y se activa el potenciador
                 if bloque.tipo == "potenciador":
                     bloque.activar_potenciador()
                     bloque.kill()
                     continue
 
-                # Si es un bloque "metal", no hacer nada más
                 if bloque.tipo == "metal":
                     continue
 
-                # Reducir los impactos en el bloque y eliminarlo si es necesario
                 bloque.impactos -= 1
                 if bloque.impactos == 0:
                     bloque.kill()
@@ -460,7 +416,6 @@ def mainAI():
 
         PANTALLA.fill(NEGRO)
 
-        # Dibujar y actualizar bloques
         bloques.draw(PANTALLA)
         todos_los_sprites.draw(PANTALLA)
 
@@ -471,9 +426,9 @@ def mainAI():
         pygame.display.flip()
         reloj.tick(60)
 
-        if VIDAS == 1 and bola.rect.top >= ALTO:  # Si la vida es 1, termina el juego
+        if VIDAS == 1 and bola.rect.top >= ALTO:
             quitar_vida()
-        elif VIDAS > 1 and bola.rect.top >= ALTO:  # Verifica si hay más de 1 vida antes de restar
+        elif VIDAS > 1 and bola.rect.top >= ALTO:
             quitar_vida()
 
     game_over()
